5996e4232243dc4727a3b2d9d5c179c3f0286d87
Ryan Rachmad Hidayat <90826637+Ryan-Rachmad-Hidayat@users.noreply.github.com>
2025-06-11 16:13:20 +0700

    Fix App

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-import os
 
 app = Flask(__name__)
 
